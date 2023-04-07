9455842f77cfd1bb9902f0d633dad6f5057ca63c
Tauri <tauri.saar@voco.ee>
2023-04-07 15:40:21 +0300

    Added CSS to date

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,14 +1,16 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props){
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     const month = props.date.toLocaleString('en-US', {month: 'long'});
     const year = props.date.getFullYear();
 
     return (
-        <div className="expense-item">
+        <div className="expense-date">
             <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
+                <div className="expense-date__month">{month}</div>
+                <div className="expense-date__day">{day}</div>
+                <div className="expense-date__year">{year}</div>
             </div>
         </div>
     )

b23d7b0cc7efcce0bc974126c8346d1cf1a00c25
Tauri <tauri.saar@voco.ee>
2023-04-16 17:17:39 +0300

    useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,13 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title);
 
     const clickHandler = () => {
-        console.log("Clicked!");
+        setTitle("Like Ooh-Ahh by TWICE");
+        console.log(title);
     }
 
     return (
